33589af849b3d9f3947f0cc30e1001fc87f17e15
Dexter <71167911+soham-basak@users.noreply.github.com>
2024-04-26 18:47:01 +0530

    feat: :sparkles: errorMiddleware

--- src/api.js
+++ src/api.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const itemRoutes = require("./api/routes/items");
+const errorMiddleware = require("../middlewares/errorMiddleware/errorMiddleware");
 
 const app = express();
 app.use(bodyParser.json());
+
+// Use error middleware for every route
+app.use(errorMiddleware);
 
 // Use item routes
 app.use("/items", itemRoutes);
